83580b1a70582496f03479fc451967bf3d83315b
Carré <74803006+MyriamCarre-Git@users.noreply.github.com>
2022-02-20 18:55:11 -0500

    Movement, Courage and Light
    -Character movements and directions
    -Light gives courage
    -Courage depletes in darkness
    -Less courage you have the  slower you move

---
+++ GameJam_Brackeys2022_1/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] GameObject Player;
+    PlayerController player;
+    [SerializeField] GameObject LightObject;
+    LightScript lightScript;
+
+
+    private void Awake()
+    {
+        //LightObject = FindObjectOfType<GameObject LightObject>();
+        player = Player.GetComponent<PlayerController>();
+        //lightScript = LightObject.GetComponent<LightScript>();
+    }
+
+    private void Update()
+    {
+        //InLight();
+        //player.CourageDepletion();
+    }
+
+   
+}
---
+++ GameJam_Brackeys2022_1/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player")]
+    public float baseCourage;
+    public float currentCourage;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float currentSpeed;
+    [SerializeField] private float minimumSpeed;
+    [SerializeField] float rotationSpeed;
+
+    Vector2 axisInput;
+    public bool isCourageFilling = false;
+    private bool isInLight = false;
+
+    [Header("Références")]
+    [SerializeField] GameObject GameManager;
+    GameManager gameManager;
+    Rigidbody2D rb;
+    //Animator anim;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gameManager = GameManager.GetComponent<GameManager>();
+
+        currentSpeed = moveSpeed;
+        currentCourage = baseCourage;
+
+       
+    }
+
+    void Update()
+    {
+        axisInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        CourageDepletion();
+        Vitesse();
+        FacingDirection();
+
+        if (isInLight)
+        {
+            CourageFill();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+
+    private void CourageDepletion()
+    {
+        if(currentCourage > 0 && !isCourageFilling)
+        {
+            currentCourage -= 1 * Time.deltaTime;
+        }
+    }
+    
+    public void CourageFill()
+    {
+        if (currentCourage < baseCourage)
+        {
+            currentCourage += 1 * Time.deltaTime;
+            isCourageFilling = true;
+        }
+    }
+   
+    private void PlayerMovement()
+    {
+        rb.velocity = axisInput.normalized * currentSpeed * Time.deltaTime;
+    }
+
+    private void Vitesse()
+    {
+        currentSpeed = Mathf.Clamp(currentCourage * 15, minimumSpeed, moveSpeed);
+    }
+    private void FacingDirection()
+    {
+        if (axisInput != Vector2.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, axisInput);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LightCourage"))
+        {
+            isInLight = true;
+            //CourageFillCoroutine = StartCoroutine(CourageFill());
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LightCourage"))
+        {
+            isCourageFilling = false;
+            isInLight = false;
+            
+        }
+    }
+}
