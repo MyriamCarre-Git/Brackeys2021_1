3705b71c87847299b999f93448b57e4ef3abca3a
Carré <74803006+MyriamCarre-Git@users.noreply.github.com>
2022-02-20 20:16:20 -0500

    Camera Follow

---
+++ GameJam_Brackeys2022_1/Assets/_Scripts/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [Range(1, 10)] [SerializeField] float lerpSpeed;
+
+
+    void FixedUpdate()
+    {
+        CameraFollow();
+    }
+
+    private void CameraFollow()
+    {
+        Vector3 LerpPosition = Vector2.Lerp(transform.position, target.position, lerpSpeed * Time.fixedDeltaTime);
+        transform.position = new Vector3(LerpPosition.x, LerpPosition.y, -10);
+    }
+}
--- GameJam_Brackeys2022_1/Assets/_Scripts/GameManager.cs
+++ GameJam_Brackeys2022_1/Assets/_Scripts/GameManager.cs
@@ -7,21 +7,15 @@ public class GameManager : MonoBehaviour
     [Header("References")]
     [SerializeField] GameObject Player;
     PlayerController player;
-    [SerializeField] GameObject LightObject;
-    LightScript lightScript;
-
+    
 
     private void Awake()
     {
-        //LightObject = FindObjectOfType<GameObject LightObject>();
         player = Player.GetComponent<PlayerController>();
-        //lightScript = LightObject.GetComponent<LightScript>();
     }
 
     private void Update()
     {
-        //InLight();
-        //player.CourageDepletion();
     }
 
    
