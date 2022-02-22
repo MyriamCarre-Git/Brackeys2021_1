e2e9c92422102b935b1502c04a3188a7f1bd0134
Carré <74803006+MyriamCarre-Git@users.noreply.github.com>
2022-02-22 08:57:27 -0500

    Music, Level 1

---
+++ GameJam_Brackeys2022_1/Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource effectsSource;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        effectsSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+}
